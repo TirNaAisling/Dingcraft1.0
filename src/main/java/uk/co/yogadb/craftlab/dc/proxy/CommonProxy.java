4e607894891f88835ba2d09910bd1fa183297bd2
Unknown <rod.t.marsh@gmail.com>
2017-10-17 15:13:10 +0100

    Initial Mod Setup

---
+++ src/main/java/uk/co/yogadb/craftlab/dc/proxy/CommonProxy.java
@@ -0,0 +1,5 @@
+package uk.co.yogadb.craftlab.dc.proxy;
+
+public class CommonProxy {
+
+}
---
+++ src/main/java/uk/co/yogadb/craftlab/dc/util/Reference.java
@@ -0,0 +1,11 @@
+package uk.co.yogadb.craftlab.dc.util;
+
+public class Reference {
+	public static final String MOD_ID = "dc";
+	public static final String NAME = "Dingcraft Mod";
+	public static final String VERSION = "1.0";
+	
+
+	public static final String COMMON_PROXY = "uk.co.yogadb.craftlab.dc.proxy.CommonProxy";
+	public static final String CLIENT_PROXY = "uk.co.yogadb.craftlab.dc.proxy.ClientProxy";
+}
